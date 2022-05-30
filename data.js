6cfdea3d3708651384f633b115ef741f3bebc4ed
Dina <dinagiacobbi@gmail.com>
2022-05-30 06:28:10 -0800

    Initial Commit

---
+++ data.js
@@ -0,0 +1,23 @@
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
