f95840fc84f8f5279c0684c10d20331809a25dbc
Dina <dinagiacobbi@gmail.com>
2022-05-30 07:51:59 -0800

    Dark or Light Mode Clock

--- script.js
+++ script.js
@@ -7,25 +7,39 @@ const toggle = document.querySelector('.toggle')
 
 toggle.addEventListener('click', (e) => {
     const html = document.querySelector('html')
-    if(html.classList.contains('dark')) {
+    if (html.classList.contains('dark')) {
         html.classList.remove('dark')
-        e.target.innerHTML = 'Dark mode'
+        e.target.innerHTML = 'Dark Mode'
     } else {
         html.classList.add('dark')
-        e.target.innerHTML = "Light mode"
+        e.target.innerHTML = 'Light Mode'
     }
 })
 
 function setTime() {
     const time = new Date();
-    const month = time.getMonth();
-    const day = time.getDay();
-    const hours = time.getHours;
-    const hoursForClock = hours % 12
+    const month = time.getMonth()
+    const day = time.getDay()
+    const date = time.getDate()
+    const hours = time.getHours()
+    const hoursForClock = hours >= 13 ? hours % 12 : hours;
     const minutes = time.getMinutes()
     const seconds = time.getSeconds()
+    const ampm = hours >= 12 ? 'PM' : 'AM'
 
-    hourEl.style.transform = `translate(-50%, -100%) rotate(30deg)`
+    hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 12, 0, 360)}deg)`
+    minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 60, 0, 360)}deg)`
+    secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 60, 0, 360)}deg)`
+
+    timeEl.innerHTML = `${hoursForClock}:${minutes < 10 ? `0${minutes}` : minutes} ${ampm}`
+    dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`
+}
+
+// TRAVERSY MEDIA: via StackOverflow https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
 setTime()
+
+setInterval(setTime, 1000)
